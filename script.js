1fff308ecf746de527b6c078f25e104956db8052
Abhinav Pandey <abhinavpandey56393@gmail.com>
2024-03-16 12:57:33 +0530

    added new changes

---
+++ script.js
@@ -0,0 +1,13 @@
+// let gamenum = 10;
+// let num = prompt("guess the number :");
+// let i = 1;
+// while(i < 100){
+//        if(num == gamenum){
+//         console.log("you win the game >");
+//     }else{
+//         num = prompt("guess the number :");
+//        i++;
+//     }
+//     i = 100;
+// }
+
